bbea232d57cf23cc4076e889e6326506266349fe
Azad <azad@azad.com>
2019-08-01 20:17:36 +0600

    Modified

--- azad-jobs.php
+++ azad-jobs.php
@@ -1,13 +1,13 @@
 <?php
 /* 
-Plugin Name: WP Job Listing
+Plugin Name: Azad Jobs
 Description: Description will go here...
 Plugin URi: gittechs.com/plugin/azad-custom-login 
 Author: Md. Abul Kalam Azad
 Author URI: gittechs.com/author
 Author Email: webdevazad@gmail.com
 Version: 0.0.0.1
-Text Domain: wp-job-listing
+Text Domain: azad-jobs
 */ 
 
 // EXIT IF ACCESSED DIRECTLY
